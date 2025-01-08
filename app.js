66beb78adadeacd1f30c9126da52d866d4516447
francescocifaldi <francesco.cifaldi@gmail.com>
2025-01-08 14:49:34 +0100

    ADD static images and notFound middleware

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const notFound = require('./middlewares/notFound')
+
+app.use(express.static('public'))
+
+app.get('/',(req,res) =>{
+    res.send('OK')
+})
+
+app.use(notFound)
 
 app.listen(port, () => {
     console.log(`Server on port ${port}`)
